c219cd33a0b5f05a80f18825563132d328248e81
Renzo Carara <renzo.carara@libero.it>
2020-04-07 18:54:29 +0200

    release iniziale - sito IN PROGRESS

---
+++ contact_result.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="Description" content="Renzo Carara - web developer - Portfolio - Invio messaggio" />
+    <meta name="author" content="Renzo Carara" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Tomorrow&display=swap" rel="stylesheet">
+
+    <!-- favicon -->
+    <link rel="icon" href="favicon/favicon.ico" />
+    <link rel="shortcut icon" href="favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml">
+
+    <!-- animate.css -->
+    <link rel="stylesheet" href="node_modules/animate.css/animate.min.css">
+    <!-- aos -->
+    <link rel="stylesheet" href="node_modules/aos/dist/aos.css" />
+    <!-- my css -->
+    <link rel="stylesheet" href="public/css/style.css" />
+
+    <title>Renzo Carara Web Developer Portfolio - Invio messaggio</title>
+
+</head>
+<?php 
+
+if (isset($_GET)){
+
+    switch ($_GET['sent']) {
+
+        case 'true':
+        // la pagina contact.php ha restituito "success", mail inviata
+            $message = "<span class='light-color'><strong>Invio completato.</strong></span><br>
+                    Risponderò nel più breve tempo possibile. <br>
+                    Grazie.";
+        break;
+        
+        case 'false':
+            $message = "ATTENZIONE: invio del messaggio non riuscito. Si prega di riprovare.";
+        break; 
+        
+        case 'data_error':
+            $message = "ATTENZIONE: i dati inseriti non sono corretti. Si prega di riprovare.";
+        break; 
+        
+        case 'antispam':
+            $message = "ATTENZIONE: verifica antispam fallita! Si prega di riprovare.";
+        break; 
+
+        case 'captcha_nok':
+            $message = "ATTENZIONE: captcha non spuntato! Si prega di riprovare.";
+        break;
+    
+    default:
+            $message = "";
+        break;
+
+    } 
+}
+?>
+<body>
+    <nav class="navbar bg-info bg-overlay navbar-expand-lg navbar-dark">
+        <div class="container">
+            <div>
+                <!-- logo  e titolo -->
+                <a class="navbar-brand" href="http://renzocarara.it"><img class="logo" src="public/assets/images/rc-logo.png" alt="renzocarara.it logo" />
+                <h4 class="text-light d-inline-block">renzocarara.it</h4>
+                </a>
+            </div>
+        </div>
+    </nav>
+
+    <section class="section5 bg-dark text-light">
+        <div class="container">
+
+            <h2><span class="light-color">I</span>nvio messaggio</h2>
+            <div class="container mt-3">
+                <div class="row">
+                    <div class="col-12 col-md-8 col-lg-4 card text-secondary">
+                        <div  class="card-body">
+                            <h5 class="card-title text-dark">Esito:</h5>
+                            <p class="card-text"><?php echo $message ?></p>
+                        </div>
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+       
+            <a class="text-light h4" href="https://renzocarara.it" data-toggle="tooltip" data-placement="bottom" title="Torna in Homepage">
+            <i class="fas fa-arrow-alt-circle-left fa-2x mt-2"></i> Back
+            </a>
+
+        </div> <!-- container -->
+    </section>
+
+    <footer class="bg-info bg-overlay-footer text-light">
+        <div class="container ">
+            <div class="row">
+                <div class="col-12">
+                    <div class="mt-3 d-inlne-block">
+                        <a href="https://www.iubenda.com/privacy-policy/11546089" class="iubenda-white iubenda-embed iub-legal-only">Privacy Policy</a>
+                        <script type="text/javascript">(function (w, d) { var loader = function () { var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s, tag); }; if (w.addEventListener) { w.addEventListener("load", loader, false); } else if (w.attachEvent) { w.attachEvent("onload", loader); } else { w.onload = loader; } })(window, document);</script>
+                    </div>
+                    <div class="mt-1 d-inline-block">
+                        <a href="https://www.iubenda.com/privacy-policy/11546089/cookie-policy" class="iubenda-white iubenda-embed">Cookie Policy</a>
+                        <script type="text/javascript">(function (w, d) { var loader = function () { var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s, tag); }; if (w.addEventListener) { w.addEventListener("load", loader, false); } else if (w.attachEvent) { w.attachEvent("onload", loader); } else { w.onload = loader; } })(window, document);</script>
+                    </div>
+                    <div class="mb-1">
+                        Copyright &copy; 2020 - Renzo Carara<br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- my js file -->
+    <script src="public/js/main.js"></script>
+
+</body>
+
+</html>
+
+
+                            
---
+++ src/js/app.js
@@ -0,0 +1,166 @@
+// requires e inizializzazioni
+var $ = require('jquery');
+
+require('../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
+
+// abilito Boostrap tooltip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// abilito Boostrap Popover
+$(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+// simplebar - customize scrollbars
+require('simplebar');
+
+
+$(document).ready(function () {
+
+    // applico o rimuovo uno sfondo alla navbar in base allo scroll della pagina
+    handleNavbarOnScroll();
+
+    // faccio sparire il menu hamburger quando clicco su una voce del menu o
+    // sul bottone BTT (back to top)
+    closeHamburgerMenu();
+
+    // gestisco lo sfondo della navbar al click sull'icona hamburger
+    handleClickOnHamburgerMenu();
+
+    // smooth scoll per i link interni alla pagina
+    internalLinkSmoothScroll();
+
+    // gestisco la visualizzazione del bottone BackToTop (BTT)
+    handleBTTButton();
+
+    // visualizzo lo spinner quando l'utente preme "Invia"
+    showContactSpinner();
+
+});
+
+
+// -------------------------------- FUNCTIONs ---------------------------------
+
+function handleNavbarOnScroll() {
+    // DESCRIZIONE:
+    // applica o rimuove un background color 'non-trasparente' per la navbar,
+    // quando l'utente scrolla la pagina
+
+    var navbar = $(".navbar");
+    $(window).scroll(function () {
+        var positionFromTop = $(this).scrollTop();
+
+        // quando l'utente inizia a scrollare verso il basso
+        // rendo lo sfondo della navbar visibile, anzichè trasparente, gli do' un colore
+        if (positionFromTop > 15) {
+            // NOTA: bg-info è una classe (colore) di Bootstrap
+            navbar.addClass('bg-info navbar-shadow');
+            // rendo visibile anche il site name "renzocarara.it"
+            $('#site-name').addClass('d-inline-block').removeClass('d-none');
+
+        } else if ($(".navbar-toggler").hasClass('collapsed')) {
+            // il menu hamburger non è aperto, cioè non ha classe collapsed e l'utente
+            // ha scrollato quasi fino in cima. allora rimuovo il colore di sfondo
+            navbar.removeClass('bg-info navbar-shadow');
+            // rimuovo anche il site name "renzocarara.it"
+            $('#site-name').removeClass('d-inline-block').addClass('d-none');
+
+        }
+    });
+
+}
+
+function closeHamburgerMenu() {
+    // DESCRIZIONE:
+    // al click chiudo (collasso) le voci dell'hamburger menu
+
+    // intercetto click sulle voci del menu della navbar (eccetto la voce dropdown menu)
+    // sulle voci del dropdown menu
+    // e sul BTT button
+
+    // DA VERIFICARE SINTASSI....
+    // $('.nav-item:not(.dropdown), .dropdown-item, .scroll-to-top').on('click', function () {
+
+    $('.nav-item:not(.dropdown), .dropdown-item, .scroll-to-top').click(function () {
+        $('#navbar-menu').collapse('hide');
+    });
+}
+
+function handleClickOnHamburgerMenu() {
+    // DESCRIZIONE:
+    // al click sull'icona hamburger setto il background color della navigation bar
+
+    // intercetto click sull'icona hamburger
+    $('.navbar-toggler').click(function () {
+        if (!($('.navbar').hasClass('bg-info'))) {
+            // se non ce l'ha già, applico lo sfondo solido alla navbar
+            $('.navbar').addClass('bg-info navbar-shadow');
+        }
+    });
+}
+
+function internalLinkSmoothScroll() {
+    // DESCRIZIONE:
+    // versione w3schools, applico uno smooth scroll quando viene cliccato un link interno alla pagina
+
+    // Add smooth scrolling to internal links with class smooth-link
+    $("a.smooth-link").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number
+            // of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+}
+
+function handleBTTButton() {
+    // DESCRIZIONE:
+    // gestisco la visualizzazione del bottone per tornare in cima alla pagina
+
+    // visualizza e nasconde il bottone "back to top in base allo scroll
+    var scrollTopButton = $(".scroll-to-top");
+    $(window).scroll(function () {
+        var topPos = $(this).scrollTop();
+        if (topPos > 500) {
+            // visualizzo il bottone back to top
+            $(scrollTopButton).addClass("d-inline-block").remove("d-none");
+        } else {
+            // nascondo il bottone
+            $(scrollTopButton).removeClass("d-inline-block").addClass("d-none");
+        }
+    });
+}
+
+function showContactSpinner() {
+    // DESCRIZIONE:
+    // visualizza lo spinner dopo che il form è stato "submittato" (tutti i campi compilati)
+
+    $("#contact-form").submit(function (event) {
+
+        // qui si potrebbe fare un prevent event.preventDefault(); per bloccare il submit
+        // fare controlli di validazione sui campi del form
+        // se tutto ok fare un $("#contact-form").submit
+        // altrimenti buttar fuori un errore a video
+
+        // se il form è stato "submittato" allora visualizzo lo spinner e cambio la dicitua del bottone
+        $('#contact-send-btn-txt').text("Attendi...");
+        $('#contact-spinner').removeClass("hidden").addClass("visible");
+    });
+}
